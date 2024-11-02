4270574f1e74e80e20e50c51835c40cd72846694
Kumar Divyanshu <87257292+divyanshuk72@users.noreply.github.com>
2024-11-02 11:59:44 +0530

    Development completed

--- src/components/ListTransfer/ListTransfer.jsx
+++ src/components/ListTransfer/ListTransfer.jsx
@@ -1,5 +1,38 @@
+import { useNavigate } from "react-router-dom";
+
 const ListTransfer = () => {
-  return <div>ListTransfer</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center gap-12 text-white mt-[50px]">
+        <h1 className="text-4xl">List Transfer</h1>
+        <div className="flex justify-center items-center gap-8">
+          <div className="bg-customRed w-[200px] h-[400px] rounded-md p-4">
+            List 1
+          </div>
+          <div className="flex flex-col justify-center items-center gap-3">
+            <button className="rounded-[50%] bg-customRed p-2 w-10 hover:bg-red-400">
+              {">"}
+            </button>
+            <button className="rounded-[50%] bg-customRed p-2 w-10 hover:bg-red-400">
+              {"<"}
+            </button>
+          </div>
+          <div className="bg-customRed w-[200px] h-[400px] rounded-md p-4">
+            List 1
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            navigate("/");
+          }}
+          className="p-2 bg-customRed border-none rounded-lg text-white text-lg cursor-pointer my-5 mx-5"
+        >
+          Back
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default ListTransfer;
